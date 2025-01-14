ea3070649cc4d54d38c6b9430ef6c5083efbd583
Moksh Keloo <moksh@integratedwebworks.com>
2025-01-14 16:05:26 -0500

    Refactor index layout to use NavigationContainer and remove unused components; update dependencies for navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,12 @@
-import BasicCoreComponents from "@/components/CoreComponents/BasicCoreComponents";
-import FlexLayout from "@/components/CoreComponents/FlexLayout";
-import ScrollViewComponent from "@/components/CoreComponents/ScrollViewComponent";
-import StylingComponents from "@/components/CoreComponents/StylingComponents";
-import TextInputComponent from "@/components/CoreComponents/TextInputComponent";
-import TouchableComponent from "@/components/CoreComponents/TouchableComponent";
-import TodoAppComponent from "@/components/TodoAppComponents/TodoAppComponent";
+import { NavigationContainer } from "@react-navigation/native";
 import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <ScrollView
-      nestedScrollEnabled={true}
-      contentContainerClassName="flex p-4 my-4"
-    >
-      {/* <Text className="text-3xl font-bold text-center my-4">
-        Core Components
-      </Text>
-      <BasicCoreComponents />
-      <TextInputComponent />
-      <ScrollViewComponent />
-      <StylingComponents />
-      <FlexLayout />
-      <TouchableComponent /> */}
-
-      <TodoAppComponent />
-    </ScrollView>
+    <NavigationContainer>
+      <View className="flex p-4 my-4">
+        <Text className="text-3xl">React Native Navigation</Text>
+      </View>
+    </NavigationContainer>
   );
 }
